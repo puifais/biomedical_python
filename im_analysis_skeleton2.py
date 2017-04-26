1f523ac7b34fe20199b8be612ef2cc61325eb8c7
Puifai Santisakultarm <puifais@users.noreply.github.com>
2017-04-26 13:35:44 -0700

    open file and show metadata and image using bio formats

--- im_analysis_skeleton2.py
+++ im_analysis_skeleton2.py
@@ -2,74 +2,100 @@
 # coding: utf-8
 
 # # im_analysis_skeleton2.py
-#
-# This is written to serve as a skeleton for anyone who want to implement bioformats in Python2
+# 
+# This is written to serve as an example for anyone who want to implement bioformats in Python2
 
-# In[41]:
+# In[56]:
 
 import Tkinter as Tk, tkFileDialog
 import os, sys
 import javabridge as jv
 import bioformats as bf
-import bioformats.omexml as ome
 import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 import numpy as np
-#from PIL import Image, ImagePath
 
 
-# In[5]:
-
-# debug
-#get_ipython().system(u'python --version')
-#get_ipython().system(u'which python')
-#get_ipython().system(u'jupyter --version')
-#get_ipython().system(u'which jupyter')
-
-
-# ##### Start Java Virtual Machine.
-# Note that if you try to do this in Python3, the Java Virtual Machine gives an error.
+# ##### Start Java Virtual Machine. 
+# Note that if you try to do this in Python3, the Java Virtual Machine (javabridge version 1.0.14) gives an error.
 # Also, max_heap_size was set to be 3/4 of my computer's RAM
 
-# In[6]:
+# In[2]:
 
 jv.start_vm(class_path=bf.JARS, max_heap_size='12G')
 
 
 # ##### User select image file to work with
 
-# In[39]:
+# In[59]:
 
-#hiding root alllows file diaglog GUI to be shown without any other GUI elements
 root = Tk.Tk()
-root.withdraw()
-file_full_path = tkFileDialog.askopenfilename()
+root.withdraw() #hiding root alllows file diaglog GUI to be shown without any other GUI elements
+#file_full_path = tkFileDialog.askopenfilename()
+file_full_path = '/Users/puifai/Dropbox (Biophotonics)/Biophotonics Team Folder/Dropbox (Biophotonics - Puifai)/mixing_work_and_play/skeleton/Cell1.lsm'
 filepath, filename = os.path.split(file_full_path)
 os.chdir(os.path.dirname(file_full_path))
 
-print('opening:  %s' %filename)
-im = bf.ImageReader(file_full_path)
-md = bf.get_omexml_metadata(file_full_path)
-o = ome.OMEXML()
-
-
-# ##### Show image
-
-# In[43]:
-
-#implot = plt.imshow(im)
+print('User Selected:  %s' %filename)
 
 
 # ##### Show metadata
+# I am using Python-bioformats version 1.1.0 and, as of 4/26/2016, there is a bug in omexml.py. To fix this bug, 
+# - Go to your path that contains omexml.py (/anaconda/envs/env2_bioformats/lib/python2.7/site-packages/bioformats/ in my case)
+# - Delete omexml.pyc
+# - Then open omexml.py and change line 318 from 'isinstance(xml, str):' to 'isinstance(xml, basestring)'
+# 
+# If you don't do this, you may get an error that says "ascii codec can't encode character"
 
-# In[46]:
+# In[60]:
 
-print(o.image(0).AcquisitionDate)
-print(o.image(0).Name)
-print(o.image(0).Pixels)
+md = bf.get_omexml_metadata(file_full_path)
+ome = bf.OMEXML(md)
+iome = ome.image(0) # e.g. first image
+#print(ome.image_count)
+
+print('Image Name:  %s' %iome.get_Name())
+print('Image ID:  %s' %iome.get_ID()) #what is image ID?
+print('Acquisition Date:  %s'  %iome.AcquisitionDate)
+print('')
+
+print('Bit Depth:  %s' %iome.Pixels.get_PixelType())
+print('XYZ Dimensions:  %s x %s x %s pixels' %(iome.Pixels.get_SizeX(),iome.Pixels.get_SizeY(),iome.Pixels.get_SizeZ()))
+print('Time Points:  %s' %iome.Pixels.get_SizeT())
+print('DimensionOrder:  %s' %iome.Pixels.DimensionOrder)
+#print('get_DimensionOrder:  %s' %iome.Pixels.get_DimensionOrder()) #what is the difference between get_DimensionOrder() and DimensionOrder?
+print('Channels:  %s' %iome.Pixels.get_SizeC())
+print('Ch1:  %s' %iome.Pixels.Channel(0).Name)
+print('Ch2:  %s' %iome.Pixels.Channel(1).Name)
+
+
+# ##### Show image
+# In this example, the array dimensions are ordered as ZYXC. There's no T dimension. You can order things however you want. Just need to restructure the loop(s)
+
+# In[58]:
+
+reader = bf.ImageReader(file_full_path)
+
+raw_data = []
+for z in range(iome.Pixels.get_SizeZ()):
+    raw_image = reader.read(z=z, series=0, rescale=False)
+    raw_data.append(raw_image)
+    
+raw_data = np.array(raw_data)
+print('Your array of image has the shape:  %s' %str(raw_data.shape))
+
+plt.imshow(raw_data[16, :, :, 0], cmap=cm.gray)
+plt.show()
 
 
 # ##### Close Java Virtual Machine at the end
 
-# In[6]:
+# In[61]:
 
 jv.kill_vm()
+
+
+# In[ ]:
+
+
+
